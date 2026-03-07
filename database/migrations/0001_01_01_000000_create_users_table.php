dc43f4acc105e7e64e9175c42251ad1ba5132a11
balqeeszeyad <balqeeszeyad562@gmail.com>
2026-03-08 00:51:56 +0300

    COMMENT:Database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('mobile')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['buyer','administrator']);
             $table->rememberToken();
             $table->timestamps();
         });
